373ebefa92c003ea4340e63ea347dd70697c73da
Dell <Dell@DESKTOP-FM8464J>
2024-01-10 07:23:31 +0545

    Add project files.

---
+++ TryCatchC#/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            //code might throw an exception
+            int result = 10 / int.Parse("0");
+        }
+        catch (DivideByZeroException ex)
+        {
+            //Handle specific exception
+            Console.WriteLine("Error:" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("General Error:" + ex.Message);
+        }
+    }
+}
